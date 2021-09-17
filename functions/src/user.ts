9b2bc21c429d4ae83326df0b3652d5de32925d39
alkaitagi <alkaitagi@outlook.com>
2021-09-17 20:44:09 +0300

    Fix functions export

--- functions/src/user.ts
+++ functions/src/user.ts
@@ -1,17 +1,22 @@
 import * as firebase from "firebase-admin";
 import * as functions from "firebase-functions";
 
-export const addUser = functions.auth.user().onCreate((user) =>
-  firebase.firestore()
-      .doc("users/" + user.uid)
-      .set({
-        displayName: user.displayName,
-        photoURL: user.photoURL,
-      })
-);
+export const addUser = functions
+    .region("europe-central2")
+    .auth.user()
+    .onCreate((user) =>
+      firebase.firestore()
+          .doc("users/" + user.uid)
+          .set({
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          })
+    );
 
-export const deleteUser = functions.auth.user().onDelete((user) =>
-  firebase.firestore()
-      .doc("users/" + user.uid)
-      .delete()
-);
+export const deleteUser = functions.region("europe-central2")
+    .auth.user()
+    .onDelete((user) =>
+      firebase.firestore()
+          .doc("users/" + user.uid)
+          .delete()
+    );
