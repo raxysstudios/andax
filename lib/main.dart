766a343899de4381540cd91ee2df9e5898048534
alkaitagi <alkaitagi@outlook.com>
2021-09-17 18:22:52 +0300

    Add Firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'scenarios/scenarios_screen.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp();
   WidgetsFlutterBinding.ensureInitialized();
   runApp(App());
 }
