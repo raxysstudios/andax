78b9f1bc17b73dcc2ba29b5ff470a72bf846f93f
alkaitagi <alkaitagi@outlook.com>
2021-09-17 20:57:06 +0300

    Initialize admin in functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,4 @@
+import * as admin from "firebase-admin";
+admin.initializeApp();
+
 export * from "./user";
-
-
-// import * as functions from "firebase-functions";
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
