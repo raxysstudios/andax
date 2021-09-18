ca9b6e2cc1ffe8cf429fe01a86e68b9cf8c81847
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2021-09-18 18:57:14 +0300

    Use different classes for transition types

--- lib/models/translation_asset.dart
+++ lib/models/translation_asset.dart
@@ -1,11 +1,43 @@
-class TranslationAsset {
-  String id;
-  String? text;
-  String? audioUrl;
+import 'package:andax/models/content_meta_data.dart';
 
-  TranslationAsset({
-    required this.id,
-    this.text,
-    this.audioUrl,
+enum AssetKind {
+  Message,
+  Scenario,
+  Author,
+}
+
+class TranslationAsset {
+  final AssetKind assetKind;
+  final ContentMetaData metaData;
+
+  const TranslationAsset({
+    required this.metaData,
+    required this.assetKind,
   });
+}
+
+class ScenarioAsset extends TranslationAsset {
+  final String title;
+  final String description;
+
+  const ScenarioAsset({
+    required this.title,
+    required this.description,
+    required ContentMetaData metaData,
+    required AssetKind assetKind,
+  })  : assert(assetKind == AssetKind.Scenario),
+        super(metaData: metaData, assetKind: assetKind);
+}
+
+class MessageAsset extends TranslationAsset {
+  final String text;
+  final String audioUrl;
+
+  const MessageAsset({
+    required this.text,
+    required this.audioUrl,
+    required ContentMetaData metaData,
+    required AssetKind assetKind,
+  })  : assert(assetKind == AssetKind.Message),
+        super(metaData: metaData, assetKind: assetKind);
 }
