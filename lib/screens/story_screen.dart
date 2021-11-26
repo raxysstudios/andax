2ed9c53a6a1b6a4a52a749264ac08ae0ebc4bde4
alkaitagi <alkaitagi@outlook.com>
2021-11-26 22:39:23 +0300

    Improve loading dialogs

--- lib/screens/story_screen.dart
+++ lib/screens/story_screen.dart
@@ -1,5 +1,6 @@
 import 'package:andax/content_loader.dart';
 import 'package:andax/editor/story_editor_screen.dart';
+import 'package:andax/models/translation.dart';
 import 'package:andax/screens/crowdsourcing_screen.dart';
 import 'package:andax/screens/play_screen.dart';
 import 'package:andax/widgets/loading_dialog.dart';
@@ -21,20 +22,6 @@ class StoryScreen extends StatefulWidget {
 }
 
 class _StoryScreenState extends State<StoryScreen> {
-  Future<void> play() async {
-    final story = await loadStory(widget.info);
-    final translation = await loadTranslation(widget.info);
-    await Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => PlayScreen(
-          story: story,
-          translations: translation.assets.values.toList(),
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,7 +42,29 @@ class _StoryScreenState extends State<StoryScreen> {
         ],
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () => showLoadingDialog(context, play()),
+        onPressed: () async {
+          final data = await showLoadingDialog<List>(
+            context,
+            Future.wait([
+              loadStory(widget.info),
+              loadTranslation(widget.info),
+            ]),
+          );
+          if (data != null) {
+            await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return PlayScreen(
+                    story: data[0],
+                    translations:
+                        (data[1] as Translation).assets.values.toList(),
+                  );
+                },
+              ),
+            );
+          }
+        },
         icon: const Icon(Icons.play_arrow_rounded),
         label: const Text('Play'),
       ),
@@ -82,18 +91,22 @@ class _StoryScreenState extends State<StoryScreen> {
                         const SizedBox(width: 8),
                         IconButton(
                           onPressed: () async {
-                            final translation =
-                                await loadTranslation(widget.info);
-                            Navigator.push(
+                            final translation = await showLoadingDialog(
                               context,
-                              MaterialPageRoute(
-                                builder: (context) => CrowdsourcingScreen(
-                                  storyId: widget.info.storyID,
-                                  translations:
-                                      translation.assets.values.toList(),
-                                ),
-                              ),
+                              loadTranslation(widget.info),
                             );
+                            if (translation != null) {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => CrowdsourcingScreen(
+                                    storyId: widget.info.storyID,
+                                    translations:
+                                        translation.assets.values.toList(),
+                                  ),
+                                ),
+                              );
+                            }
                           },
                           tooltip: 'Translate story',
                           icon: const Icon(Icons.translate_rounded),
@@ -101,19 +114,27 @@ class _StoryScreenState extends State<StoryScreen> {
                         const SizedBox(width: 8),
                         IconButton(
                           onPressed: () async {
-                            final story = await loadStory(widget.info);
-                            final translation =
-                                await loadTranslation(widget.info);
-                            Navigator.push(
+                            final data = await showLoadingDialog<List>(
                               context,
-                              MaterialPageRoute(
-                                builder: (context) => StoryEditorScreen(
-                                  story: story,
-                                  translation: translation,
-                                  info: widget.info,
-                                ),
-                              ),
+                              Future.wait([
+                                loadStory(widget.info),
+                                loadTranslation(widget.info),
+                              ]),
                             );
+                            if (data != null) {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return StoryEditorScreen(
+                                      story: data[0],
+                                      translation: data[1] as Translation,
+                                      info: widget.info,
+                                    );
+                                  },
+                                ),
+                              );
+                            }
                           },
                           tooltip: 'Edit story',
                           icon: const Icon(Icons.edit_rounded),
