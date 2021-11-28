cf9be325d2480143bfabd34b44d5672e2607e832
alkaitagi <alkaitagi@outlook.com>
2021-11-28 10:35:32 +0300

    Unify node views

--- lib/editor/story_node_picker.dart
+++ lib/editor/story_node_picker.dart
@@ -1,7 +1,5 @@
 import 'package:andax/models/actor.dart';
 import 'package:andax/models/node.dart';
-import 'package:andax/models/story.dart';
-import 'package:andax/models/translation.dart';
 import 'package:andax/models/translation_asset.dart';
 import 'package:andax/widgets/modal_picker.dart';
 import 'package:andax/widgets/rounded_back_button.dart';
@@ -11,18 +9,19 @@ import 'package:provider/provider.dart';
 
 import 'story_editor_screen.dart';
 
-// TODO fix state updating.
-// TODO unify with picker.
+// TODO add picker filterting.
 
 class NarrativeEditorSliver extends StatefulWidget {
   const NarrativeEditorSliver(
     this.editor, {
     this.onSelected,
     this.selectedId,
+    this.interactive = true,
     Key? key,
   }) : super(key: key);
 
   final StoryEditorState editor;
+  final bool interactive;
   final String? selectedId;
   final ValueSetter<Node>? onSelected;
 
@@ -31,14 +30,15 @@ class NarrativeEditorSliver extends StatefulWidget {
 }
 
 class _NarrativeEditorSliverState extends State<NarrativeEditorSliver> {
-  Translation get translation => widget.editor.translation;
-  Story get story => widget.editor.story;
+  late final translation = widget.editor.translation;
+  late final story = widget.editor.story;
+  late final nodes = widget.editor.story.nodes.values.toList();
 
   var choices = <String, String>{};
 
   List<Node> computeThread() {
     var thread = <Node>{};
-    var node = story.nodes.isEmpty ? null : story.nodes.values.first;
+    var node = nodes.isEmpty ? null : nodes.first;
     while (node != null) {
       thread.add(node);
 
@@ -62,12 +62,11 @@ class _NarrativeEditorSliverState extends State<NarrativeEditorSliver> {
 
   @override
   Widget build(BuildContext context) {
-    final nodes = story.nodes.values.toList();
-    final thread = computeThread();
+    final nodes = widget.interactive ? computeThread() : this.nodes;
     return SliverList(
       delegate: SliverChildBuilderDelegate(
         (context, index) {
-          final node = thread[index];
+          final node = nodes[index];
           final actor = story.actors[node.actorId];
           final transitions = node.transitions ?? [];
           final choice = choices[node.id] ??
@@ -109,7 +108,7 @@ class _NarrativeEditorSliverState extends State<NarrativeEditorSliver> {
                 ),
                 selected: node.id == widget.selectedId,
               ),
-              if (transitions.length > 1)
+              if (widget.interactive && transitions.length > 1)
                 SizedBox(
                   height: 50,
                   child: ListView(
@@ -139,99 +138,54 @@ class _NarrativeEditorSliverState extends State<NarrativeEditorSliver> {
             ],
           );
         },
-        childCount: thread.length,
+        childCount: nodes.length,
       ),
     );
   }
-}
 
-SliverList buildNodesSliverList(
-  StoryEditorState editor,
-  ValueSetter<Node> onSelected, [
-  String? selectedId,
-  List<Node>? nodes,
-]) {
-  nodes ??= editor.story.nodes.values.toList();
-  return SliverList(
-    delegate: SliverChildBuilderDelegate(
-      (context, index) {
-        final node = nodes![index];
-        final actor = editor.story.actors[node.actorId];
-        final transitions = node.transitions ?? [];
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ListTile(
-              onTap: () => onSelected(node),
-              title: Text(
-                MessageTranslation.getText(
-                  editor.translation,
-                  node.id,
+  Widget buildTile(Node node) {
+    final actor = story.actors[node.actorId];
+    return ListTile(
+      onTap: () => widget.onSelected?.call(node),
+      title: Text(
+        MessageTranslation.getText(
+          translation,
+          node.id,
+        ),
+      ),
+      subtitle: actor == null
+          ? null
+          : Row(
+              children: [
+                Icon(
+                  actor.type == ActorType.npc
+                      ? Icons.smart_toy_rounded
+                      : Icons.face_rounded,
+                  size: 16,
+                  color: Theme.of(context).textTheme.caption?.color,
                 ),
-              ),
-              subtitle: actor == null
-                  ? null
-                  : Row(
-                      children: [
-                        Icon(
-                          actor.type == ActorType.npc
-                              ? Icons.smart_toy_rounded
-                              : Icons.face_rounded,
-                          size: 16,
-                          color: Theme.of(context).textTheme.caption?.color,
-                        ),
-                        const SizedBox(width: 4),
-                        Text(
-                          ActorTranslation.getName(
-                            editor.translation,
-                            actor.id,
-                          ),
-                        )
-                      ],
-                    ),
-              trailing: Text(
-                '#${(nodes.indexOf(node) + 1)}',
-                style: Theme.of(context).textTheme.subtitle2,
-              ),
-              selected: node.id == selectedId,
+                const SizedBox(width: 4),
+                Text(
+                  ActorTranslation.getName(
+                    translation,
+                    actor.id,
+                  ),
+                )
+              ],
             ),
-            if (transitions.length > 1)
-              SizedBox(
-                height: 50,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  padding: const EdgeInsets.all(8),
-                  children: [
-                    for (final transition in transitions)
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 4),
-                        child: InputChip(
-                          onPressed: () {},
-                          label: Text(
-                            MessageTranslation.getText(
-                              editor.translation,
-                              transition.id,
-                            ),
-                          ),
-                        ),
-                      ),
-                  ],
-                ),
-              ),
-            const Divider(),
-          ],
-        );
-      },
-      childCount: nodes.length,
-    ),
-  );
+      trailing: Text(
+        '#${(nodes.indexOf(node) + 1)}',
+        style: Theme.of(context).textTheme.subtitle2,
+      ),
+      selected: node.id == widget.selectedId,
+    );
+  }
 }
 
 Future<Node?> showStoryNodePickerSheet(
   BuildContext context, [
   String? selectedId,
 ]) {
-  final editor = context.read<StoryEditorState>();
   return showModalPicker(context, [
     const SliverAppBar(
       leading: RoundedBackButton(),
@@ -241,10 +195,11 @@ Future<Node?> showStoryNodePickerSheet(
       snap: true,
       pinned: true,
     ),
-    buildNodesSliverList(
-      editor,
-      (node) => Navigator.pop(context, node),
-      selectedId,
+    NarrativeEditorSliver(
+      context.read<StoryEditorState>(),
+      onSelected: (node) => Navigator.pop(context, node),
+      selectedId: selectedId,
+      interactive: false,
     ),
   ]);
 }
