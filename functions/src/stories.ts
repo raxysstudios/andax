7a6c7d5e33dff9755eb6a3d3946faf303f46c03e
alkaitagi <alkaitagi@outlook.com>
2022-01-28 16:13:27 +0300

    Add rating & indexing functions

--- functions/src/stories.ts
+++ functions/src/stories.ts
@@ -1,3 +1,5 @@
+/* eslint-disable require-jsdoc */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import algoliasearch from "algoliasearch";
 import * as functions from "firebase-functions";
@@ -12,9 +14,11 @@ const index = algoliasearch(
 const db = firestore();
 
 type storyRecord = {
-  storyID: string;
-  translationID: string;
-  language: string;
+  storyID: string,
+  storyAuthorID:string,
+  translationID: string,
+  translationAuthorID: string,
+  language: string,
   title: string,
   description: string,
 };
@@ -42,7 +46,7 @@ export const indexStories = functions
             .doc(`stories/${storyID}/translations/${translationID}`)
             .get()
             .then((doc) => doc.data()!);
-        await index.saveObject(
+        await index.partialUpdateObject(
         {
           storyID,
           storyAuthorID: story.metaData.authorId,
@@ -52,14 +56,62 @@ export const indexStories = functions
           title,
           description,
           tags,
-          likes: translation.metaData.likes,
         } as storyRecord,
-        {autoGenerateObjectIDIfNotExist: true}
+        {createIfNotExists: true}
         );
       }
     });
 
-// TODO
+export const updateStoryMeta = functions
+    .region("europe-central2")
+    .firestore.document(
+        "stories/{storyID}/translations/{translationID}"
+    )
+    .onWrite(async (change, context) => {
+      if (change.after.exists) {
+        const meta = change.after.data()?.metaData;
+        const likes = meta.likes ?? 0;
+        const views = meta.views ?? 0;
+
+        if (change.before.exists) {
+          const {_likes, _views} = change.before.data()?.metaData;
+          if (_likes === likes && _views === views) return;
+        }
+
+        const objectID = await index.search("", {
+          filters: "translationID:" + context.params.translationID,
+          hitsPerPage: 1,
+        }).then((r) => r.hits[0]?.objectID);
+        if (!objectID) return;
+
+        await index.partialUpdateObject({objectID, likes, views});
+      }
+    });
+
+export const indexTrendingStories = functions
+    .region("europe-central2")
+    .pubsub.schedule("every 72 hours")
+    .onRun(async () => {
+      await index.browseObjects({
+        query: "",
+        attributesToRetrieve: [
+          "storyID",
+          "translationID",
+        ],
+        batch: async (batch) => {
+          for (const hit of batch) {
+            const {storyID, translationID, objectID} = (hit as never);
+            const doc = db
+                .doc(`stories/${storyID}/translations/${translationID}`);
+            const meta = await doc.get().then((r) => r.data()!.metaData);
+            const trending = (meta.views ?? 0) - (meta.lastIndexedViews ?? 0);
+            await doc.update({"metaData.lastIndexedViews": meta.views ?? 0});
+            await index.partialUpdateObject({objectID, trending});
+          }
+        },
+      });
+    });
+
 export const countLikes = functions
     .region("europe-central2")
     .firestore.document(
@@ -67,27 +119,18 @@ export const countLikes = functions
     )
     .onWrite(async (change, context) => {
       const {storyID, translationID} =
-        (change.before.data() ?? change.after.data())!;
+      (change.before.data() ?? change.after.data())!;
       const doc = db
           .doc(`stories/${storyID}/translations/${translationID}`);
-      const srch = await index.search("", {
-        facetFilters: ["storyID:" + storyID, "translationID:" + translationID],
-        attributesToRetrieve: ["objectID", "likes"],
-        hitsPerPage: 1,
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      }).then((h) =>h.hits[0] as Record<string, any>);
       let value: firestore.FieldValue | undefined;
       if (!change.before.exists && change.after.exists) {
         value = firestore.FieldValue.increment(1);
-        srch.likes += 1;
       } else if (change.before.exists && !change.after.exists) {
         value = firestore.FieldValue.increment(-1);
-        srch.likes -= 1;
       }
       if (value) {
         await doc.update({"metaData.likes": value});
         await db.doc("users/" + context.params.userID)
             .update({"likes": value});
-        await index.partialUpdateObject(srch);
       }
     });
