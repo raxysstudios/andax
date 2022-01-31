270ef955c018433ec301217cefbcf673037b4fcd
alkaitagi <alkaitagi@outlook.com>
2022-01-31 16:52:18 +0300

    Fix index name

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -27,7 +27,7 @@ class _HomeScreenState extends State<HomeScreen> {
     int? page,
     int? hitsPerPage,
   }) async {
-    var query = algolia.instance.index('stories').query('');
+    var query = algolia.instance.index(index).query('');
     if (page != null) query = query.setPage(page);
     if (hitsPerPage != null) query = query.setHitsPerPage(hitsPerPage);
     final hits = await query.getObjects().then((s) => s.hits);
