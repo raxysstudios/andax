52b7dd4634e48cf5e9cc819873c2e7470be988f9
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-02-05 02:58:28 +0300

    Remove unused import

--- lib/models/story.dart
+++ lib/models/story.dart
@@ -1,6 +1,5 @@
 import 'package:algolia/algolia.dart';
 import 'package:andax/shared/utils.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'actor.dart';
 import 'content_meta_data.dart';
