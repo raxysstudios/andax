d7920c4076f42b4977be6bb26c042e8868ff6c00
alkaitagi <alkaitagi@outlook.com>
2022-02-17 23:43:07 +0300

    Revert paing list into stateful widget

--- lib/shared/widgets/paging_list.dart
+++ lib/shared/widgets/paging_list.dart
@@ -3,36 +3,46 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
-class PagingList<T> extends StatelessWidget {
+class PagingList<T> extends StatefulWidget {
+  const PagingList({
+    required this.onRequest,
+    required this.builder,
+    this.controller,
+    this.maxPages,
+    this.scroll,
+    Key? key,
+  }) : super(key: key);
+
+  final PagingController<int, T>? controller;
   final Widget Function(BuildContext, T, int) builder;
   final Future<List<T>> Function(int, T?) onRequest;
   final ScrollController? scroll;
   final int? maxPages;
-  final PagingController<int, T> paging;
 
-  PagingList({
-    required this.onRequest,
-    required this.builder,
-    PagingController<int, T>? controller,
-    this.maxPages,
-    this.scroll,
-    Key? key,
-  })  : paging = controller ??
-            PagingController<int, T>(
-              firstPageKey: 0,
-            ),
-        super(key: key) {
-    paging.addPageRequestListener((page) async {
-      if (maxPages != null && page >= maxPages!) {
-        paging.appendLastPage([]);
-      }
-      final items = await onRequest(page, paging.itemList?.last);
-      if (items.isEmpty) {
-        paging.appendLastPage([]);
-      } else {
-        paging.appendPage(items, page + 1);
-      }
-    });
+  @override
+  State<PagingList<T>> createState() => _PagingListState<T>();
+}
+
+class _PagingListState<T> extends State<PagingList<T>> {
+  late final PagingController<int, T> paging;
+
+  @override
+  void initState() {
+    super.initState();
+    paging = widget.controller ?? PagingController<int, T>(firstPageKey: 0);
+    paging.addPageRequestListener(
+      (page) async {
+        if (widget.maxPages != null && page >= widget.maxPages!) {
+          paging.appendLastPage([]);
+        }
+        final items = await widget.onRequest(page, paging.itemList?.last);
+        if (items.isEmpty) {
+          paging.appendLastPage([]);
+        } else {
+          paging.appendPage(items, page + 1);
+        }
+      },
+    );
   }
 
   @override
@@ -43,9 +53,9 @@ class PagingList<T> extends StatelessWidget {
       ),
       child: PagedListView<int, T>(
         pagingController: paging,
-        scrollController: scroll,
+        scrollController: widget.scroll,
         builderDelegate: PagedChildBuilderDelegate(
-          itemBuilder: builder,
+          itemBuilder: widget.builder,
         ),
       ),
     );
