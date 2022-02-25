eb06bf9cbe4dc386224d2f15ffc8c4627c693fa0
alkaitagi <alkaitagi@outlook.com>
2022-02-25 12:09:38 +0300

    Use actor editor dialog in info editor

--- lib/modules/story_editor/screens/info_editor.dart
+++ lib/modules/story_editor/screens/info_editor.dart
@@ -2,45 +2,39 @@ import 'package:andax/models/actor.dart';
 import 'package:andax/models/story.dart';
 import 'package:andax/models/translation.dart';
 import 'package:andax/models/translation_asset.dart';
+import 'package:andax/modules/story_editor/widgets/actor_editor_dialog.dart';
 import 'package:andax/shared/utils.dart';
 import 'package:andax/shared/widgets/rounded_back_button.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/narrative_list_view.dart';
 import 'narrative_editor.dart';
 
 class StoryInfoEditor extends StatefulWidget {
-  const StoryInfoEditor({
-    required this.editor,
-    Key? key,
-  }) : super(key: key);
-
-  final StoryEditorState editor;
+  const StoryInfoEditor({Key? key}) : super(key: key);
 
   @override
   State<StoryInfoEditor> createState() => _StoryInfoEditorState();
 }
 
 class _StoryInfoEditorState extends State<StoryInfoEditor> {
-  Translation get translation => widget.editor.translation;
-  Story get story => widget.editor.story;
-
   @override
   Widget build(BuildContext context) {
+    final editor = context.watch<StoryEditorState>();
+    final translation = editor.translation;
+    final story = editor.story;
+
     return Scaffold(
       appBar: AppBar(
         leading: const RoundedBackButton(icon: Icons.done_all_rounded),
         title: const Text('Story Info'),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() {
-          final id = widget.editor.uuid.v4();
-          story.actors[id] = Actor(id: id);
-          translation[id] = ActorTranslation(
-            id: id,
-            name: 'Actor #${story.actors.length}',
-          );
-        }),
+        onPressed: () => showActorEditorDialog(
+          context,
+          (r) => setState(() {}),
+        ),
         tooltip: 'Add actor',
         child: const Icon(Icons.person_add_rounded),
       ),
@@ -102,7 +96,7 @@ class _StoryInfoEditorState extends State<StoryInfoEditor> {
           ListTile(
             onTap: () async {
               final node = await showStoryNodePickerSheet(
-                widget.editor,
+                editor,
                 context,
                 story.startNodeId,
               );
@@ -121,41 +115,14 @@ class _StoryInfoEditorState extends State<StoryInfoEditor> {
           const Divider(),
           for (final actor in story.actors.values)
             ListTile(
-              leading: IconButton(
-                onPressed: () => setState(() {
-                  actor.type = actor.type == ActorType.npc
-                      ? ActorType.player
-                      : ActorType.npc;
-                }),
-                icon: Icon(actor.type == ActorType.npc
-                    ? Icons.smart_toy_rounded
-                    : Icons.face_rounded),
+              onTap: () => showActorEditorDialog(
+                context,
+                (r) => setState(() {}),
               ),
-              title: TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Actor name',
-                ),
-                initialValue: ActorTranslation.getName(
-                  translation,
-                  actor.id,
-                  '',
-                ),
-                onChanged: (s) {
-                  ActorTranslation.get(
-                    translation,
-                    actor.id,
-                  )?.name = s;
-                },
-              ),
-              trailing: IconButton(
-                onPressed: () {
-                  setState(() {
-                    story.actors.remove(actor.id);
-                    translation.assets.remove(actor.id);
-                  });
-                },
-                icon: const Icon(Icons.clear_rounded),
-              ),
+              leading: Icon(actor.type == ActorType.npc
+                  ? Icons.smart_toy_rounded
+                  : Icons.face_rounded),
+              title: Text(ActorTranslation.getName(translation, actor.id)),
             )
         ],
       ),
