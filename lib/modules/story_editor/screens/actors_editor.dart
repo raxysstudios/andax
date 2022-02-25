5601af090f13d64f58100024e58591010b26a045
alkaitagi <alkaitagi@outlook.com>
2022-02-25 14:41:23 +0300

    Create actors editor screen

---
+++ lib/modules/story_editor/screens/actors_editor.dart
@@ -0,0 +1,59 @@
+import 'package:andax/models/actor.dart';
+import 'package:andax/models/translation_asset.dart';
+import 'package:andax/modules/story_editor/screens/narrative_editor.dart';
+import 'package:andax/modules/story_editor/widgets/actor_editor_dialog.dart';
+import 'package:flutter/material.dart';
+
+class ActorsEditorScreen extends StatefulWidget {
+  const ActorsEditorScreen(
+    this.editor, {
+    Key? key,
+  }) : super(key: key);
+
+  final StoryEditorState editor;
+
+  @override
+  _ActorsEditorScreenState createState() => _ActorsEditorScreenState();
+}
+
+class _ActorsEditorScreenState extends State<ActorsEditorScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final actors = widget.editor.story.actors.values.toList();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Story actors'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => showActorEditorDialog(
+          context,
+          widget.editor,
+          (r) => setState(() {}),
+        ),
+        tooltip: 'Add actor',
+        child: const Icon(Icons.person_add_rounded),
+      ),
+      body: ListView.builder(
+          itemCount: actors.length,
+          itemBuilder: (context, i) {
+            final actor = actors[i];
+            return ListTile(
+              onTap: () => showActorEditorDialog(
+                context,
+                widget.editor,
+                (r) => setState(() {}),
+                actor,
+              ),
+              leading: Icon(actor.type == ActorType.npc
+                  ? Icons.smart_toy_rounded
+                  : Icons.face_rounded),
+              title: Text(ActorTranslation.getName(
+                widget.editor.translation,
+                actor.id,
+              )),
+            );
+          }),
+    );
+  }
+}
