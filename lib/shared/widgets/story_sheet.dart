09fcf455f45dd342157f870cfe9d9f7b37087cff
alkaitagi <alkaitagi@outlook.com>
2022-02-28 18:14:16 +0300

    Display story stats

--- lib/shared/widgets/story_sheet.dart
+++ lib/shared/widgets/story_sheet.dart
@@ -4,7 +4,10 @@ import 'package:andax/modules/story_editor/screens/story_editor.dart';
 import 'package:andax/modules/story_translator/screens/crowdsourcing.dart';
 import 'package:andax/shared/services/likes.dart';
 import 'package:andax/shared/services/story_loader.dart';
+import 'package:andax/shared/utils.dart';
 import 'package:andax/shared/widgets/gradient_cover_image.dart';
+import 'package:andax/shared/widgets/rounded_back_button.dart';
+import 'package:andax/shared/widgets/span_icon.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -26,6 +29,7 @@ Future<void> showStorySheet(BuildContext context, StoryInfo info) async {
     builder: (context, scroll) {
       return Scaffold(
         appBar: AppBar(
+          leading: const RoundedBackButton(),
           bottom: const PreferredSize(
             preferredSize: Size.fromHeight(kToolbarHeight + 3),
             child: SizedBox(),
@@ -38,18 +42,25 @@ Future<void> showStorySheet(BuildContext context, StoryInfo info) async {
             ),
           ),
           actions: [
+            Chip(
+              avatar: const Icon(Icons.visibility_rounded),
+              label: Text(info.views.toString()),
+            ),
+            const SizedBox(width: 8),
             StatefulBuilder(
               builder: (context, setState) {
-                return IconButton(
-                  onPressed: () => setState(() {
-                    liked = !liked;
-                    likeService.toggleLike(liked);
-                  }),
-                  icon: Icon(
+                final likes = info.likes + (liked ? 1 : 0);
+                return InputChip(
+                  avatar: Icon(
                     liked
                         ? Icons.favorite_rounded
                         : Icons.favorite_border_rounded,
                   ),
+                  label: Text(likes.toString()),
+                  onPressed: () => setState(() {
+                    liked = !liked;
+                    likeService.toggleLike(liked);
+                  }),
                 );
               },
             ),
@@ -173,6 +184,42 @@ Future<void> showStorySheet(BuildContext context, StoryInfo info) async {
                   style: const TextStyle(fontSize: 16),
                 ),
               ),
+            const Divider(),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: RichText(
+                overflow: TextOverflow.ellipsis,
+                text: TextSpan(
+                  style: textTheme.caption?.copyWith(
+                    fontSize: 14,
+                  ),
+                  children: [
+                    if (info.tags?.isNotEmpty ?? false) ...[
+                      const WidgetSpan(
+                        child: SpanIcon(
+                          Icons.tag_rounded,
+                          padding: EdgeInsets.only(right: 4),
+                        ),
+                      ),
+                      TextSpan(text: prettyTags(info.tags)! + '\n\n'),
+                    ],
+                    if (info.lastUpdateAt != null) ...[
+                      const WidgetSpan(
+                        child: SpanIcon(
+                          Icons.update_outlined,
+                          padding: EdgeInsets.only(right: 4),
+                        ),
+                      ),
+                      TextSpan(
+                        text: info.lastUpdateAt!
+                            .toIso8601String()
+                            .substring(0, 10),
+                      ),
+                    ],
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       );
