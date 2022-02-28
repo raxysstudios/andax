9e461042e699e142ef71f26638197602769beea5
alkaitagi <alkaitagi@outlook.com>
2022-02-28 17:28:36 +0300

    Improve story sheet UI

--- lib/shared/widgets/gradient_cover_image.dart
+++ lib/shared/widgets/gradient_cover_image.dart
@@ -4,32 +4,35 @@ class GradientCoverImage extends StatelessWidget {
   const GradientCoverImage(
     this.url, {
     this.opacity = 1,
+    this.reversed = false,
     Key? key,
   }) : super(key: key);
 
   final String? url;
   final double opacity;
+  final bool reversed;
 
   @override
   Widget build(BuildContext context) {
     if (url == null) return const SizedBox();
+    final colors = [
+      Colors.black.withOpacity(opacity),
+      Colors.transparent,
+      Colors.transparent,
+    ];
+
     return ShaderMask(
       shaderCallback: (rect) {
         return LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Colors.black.withOpacity(opacity),
-              Colors.transparent,
-              Colors.transparent,
-            ],
-            stops: const [
-              0,
-              .75,
-              1,
-            ]).createShader(
-          Rect.fromLTRB(0, 0, rect.width, rect.height),
-        );
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: reversed ? colors.reversed.toList() : colors,
+          stops: [
+            0,
+            reversed ? .25 : .75,
+            1,
+          ],
+        ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
       },
       blendMode: BlendMode.dstIn,
       child: Image.network(url!, fit: BoxFit.cover),
