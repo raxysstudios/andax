9a07d2eeea9ccc7b50af3810bdc90d76b7497aff
alkaitagi <alkaitagi@outlook.com>
2022-03-01 12:00:07 +0300

    Clean up

--- lib/modules/home/widgets/story_card.dart
+++ lib/modules/home/widgets/story_card.dart
@@ -26,11 +26,7 @@ class _StoryCardState extends State<StoryCard> {
           children: [
             Positioned.fill(
               child: widget.story.imageUrl == null
-                  ? Icon(
-                      Icons.history_edu_rounded,
-                      size: 48,
-                      color: Theme.of(context).textTheme.caption?.color,
-                    )
+                  ? const Icon(Icons.history_edu_rounded, size: 48)
                   : GradientCoverImage(
                       widget.story.imageUrl!,
                       reversed: true,
