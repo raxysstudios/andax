344467d239fe4e38f0ddfda39f88f85e0b129505
alkaitagi <alkaitagi@outlook.com>
2022-03-10 16:45:04 +0300

    Move custom popup menu button into a widget

---
+++ lib/shared/widgets/options_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class OptionItem {
+  final IconData icon;
+  final String text;
+  final VoidCallback? onTap;
+
+  OptionItem(
+    this.icon,
+    this.text, {
+    this.onTap,
+  });
+}
+
+class OptionsButton extends StatelessWidget {
+  const OptionsButton(
+    this.options, {
+    this.icon = Icons.more_vert_outlined,
+    Key? key,
+  }) : super(key: key);
+
+  final IconData icon;
+  final List<OptionItem> options;
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<void>(
+      child: Icon(icon),
+      itemBuilder: (context) {
+        return [
+          for (final option in options)
+            PopupMenuItem(
+              onTap: option.onTap,
+              child: Row(
+                children: [
+                  Icon(option.icon),
+                  const SizedBox(width: 16),
+                  Text(option.text),
+                ],
+              ),
+            ),
+        ];
+      },
+    );
+  }
+}
