f7881482e0d7a96c795c0d5e520492c80338beb2
alkaitagi <alkaitagi@outlook.com>
2022-03-14 12:46:53 +0300

    Improve tabs in node editor

--- lib/modules/editor/screens/node.dart
+++ lib/modules/editor/screens/node.dart
@@ -1,5 +1,4 @@
 import 'package:andax/models/node.dart';
-import 'package:andax/models/story.dart';
 import 'package:andax/models/transition.dart';
 import 'package:andax/models/translation_asset.dart';
 import 'package:andax/modules/editor/services/node.dart';
@@ -27,7 +26,10 @@ class NodeEditorScreen extends StatefulWidget {
   State<NodeEditorScreen> createState() => _NodeEditorScreenState();
 }
 
-class _NodeEditorScreenState extends State<NodeEditorScreen> {
+class _NodeEditorScreenState extends State<NodeEditorScreen>
+    with SingleTickerProviderStateMixin {
+  late final tabs = TabController(vsync: this, length: 3);
+
   Node get node => widget.node;
   List<Transition> get transitions => node.transitions ?? [];
 
@@ -104,85 +106,119 @@ class _NodeEditorScreenState extends State<NodeEditorScreen> {
 
   Widget buildTransitionsTab(StoryEditorState editor) {
     final transitions = node.transitions ?? [];
-    return ListView(
-      children: [
-        for (var i = 0; i < transitions.length; i++)
-          ListTile(
-            title: Text(
-              MessageTranslation.getText(
-                editor.translation,
-                transitions[i].id,
+    return Scaffold(
+      primary: false,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        icon: const Icon(Icons.control_point_rounded),
+        label: const Text('Add transition'),
+      ),
+      body: ListView(
+        children: [
+          for (var i = 0; i < transitions.length; i++)
+            ListTile(
+              title: Text(
+                MessageTranslation.getText(
+                  editor.translation,
+                  transitions[i].id,
+                ),
               ),
-            ),
-            subtitle: Text(
-              MessageTranslation.getText(
-                editor.translation,
-                transitions[i].targetNodeId,
+              subtitle: Text(
+                MessageTranslation.getText(
+                  editor.translation,
+                  transitions[i].targetNodeId,
+                ),
               ),
+              onTap: () => showTransitionEditorDialog(
+                context,
+                node,
+                transitions[i],
+              ).then((r) => setState(() {})),
+              trailing: Text('#${i + 1}'),
             ),
-            onTap: () => showTransitionEditorDialog(
-              context,
-              node,
-              transitions[i],
-            ).then((r) => setState(() {})),
-            trailing: Text('#${i + 1}'),
-          ),
-      ],
+        ],
+      ),
     );
   }
 
   Widget buildCellsTab(StoryEditorState editor) {
-    return ListView(
-      children: const [
-        Text('Cells tab'),
-      ],
+    return Scaffold(
+      primary: false,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        icon: const Icon(Icons.control_point_rounded),
+        label: const Text('Add cell write'),
+      ),
+      body: ListView(
+        children: const [
+          Text('Cells tab'),
+        ],
+      ),
     );
   }
 
   @override
   Widget build(context) {
     final editor = context.watch<StoryEditorState>();
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          leading: const RoundedBackButton(icon: Icons.done_all_rounded),
-          title: const Text('Node editor'),
-          actions: [
-            OptionsButton(
-              [
-                OptionItem.simple(
-                  Icons.delete_rounded,
-                  'Delete node',
-                  () => deleteNode(
-                    context,
-                    node,
-                    () => Navigator.pop(context),
-                  ),
+    final textTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      appBar: AppBar(
+        leading: const RoundedBackButton(icon: Icons.done_all_rounded),
+        title: const Text('Node editor'),
+        actions: [
+          OptionsButton(
+            [
+              OptionItem.simple(
+                Icons.delete_rounded,
+                'Delete node',
+                () => deleteNode(
+                  context,
+                  node,
+                  () => Navigator.pop(context),
                 ),
-                OptionItem(
-                  SwitchListTile(
-                    secondary: const Icon(Icons.play_circle_rounded),
-                    title: const Text('Story entry'),
-                    value: editor.story.startNodeId == node.id,
-                    onChanged: (r) => setState(() {
-                      editor.story.startNodeId = r ? node.id : null;
-                      Navigator.pop(context);
-                    }),
-                  ),
+              ),
+              OptionItem(
+                SwitchListTile(
+                  secondary: const Icon(Icons.play_circle_rounded),
+                  title: const Text('Story entry'),
+                  value: editor.story.startNodeId == node.id,
+                  onChanged: (r) => setState(() {
+                    editor.story.startNodeId = r ? node.id : null;
+                    Navigator.pop(context);
+                  }),
                 ),
-              ],
+              ),
+            ],
+          ),
+          const SizedBox(width: 4),
+        ],
+        bottom: TabBar(
+          controller: tabs,
+          labelColor: textTheme.bodyText1?.color,
+          unselectedLabelColor: textTheme.caption?.color,
+          tabs: const [
+            Tab(
+              icon: Icon(Icons.chat_rounded),
+              text: 'Contents',
             ),
-            const SizedBox(width: 4),
+            Tab(
+              icon: Icon(Icons.call_split_rounded),
+              text: 'Transitions',
+            ),
+            Tab(
+              icon: Icon(Icons.tune_rounded),
+              text: 'Storage',
+            ),
           ],
         ),
-        body: TabBarView(
-          children: [
-            buildContentsTab(editor),
-            buildTransitionsTab(editor),
-            buildCellsTab(editor),
-          ],
-        ),
+      ),
+      body: TabBarView(
+        controller: tabs,
+        children: [
+          buildContentsTab(editor),
+          buildTransitionsTab(editor),
+          buildCellsTab(editor),
+        ],
       ),
     );
   }
