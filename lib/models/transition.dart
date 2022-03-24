9582c669973e047f6b16822e2292d3dbeaec9803
alkaitagi <alkaitagi@outlook.com>
2022-03-24 12:57:09 +0300

    Fix condition deserialziation

--- lib/models/transition.dart
+++ lib/models/transition.dart
@@ -3,7 +3,7 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'transition.g.dart';
 
-@JsonSerializable()
+@JsonSerializable(explicitToJson: true)
 class Transition {
   final String id;
   String targetNodeId;
