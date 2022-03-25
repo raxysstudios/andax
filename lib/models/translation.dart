845f281779e1cf59779768db35c8203135a99c7c
alkaitagi <alkaitagi@outlook.com>
2022-03-25 17:52:47 +0300

    Move translation accessors into universal translation object

--- lib/models/translation.dart
+++ lib/models/translation.dart
@@ -1,20 +1,33 @@
-import 'package:andax/models/translation_asset.dart';
 import 'package:json_annotation/json_annotation.dart';
+
+import 'actor.dart';
 
 part 'translation.g.dart';
 
 @JsonSerializable(explicitToJson: true)
 class Translation {
   String language;
-  final Map<String, TranslationAsset> assets;
+  final Map<String, String> assets;
 
   Translation({
     required this.language,
-    Map<String, TranslationAsset> assets = const {},
-  }) : assets = Map<String, TranslationAsset>.of(assets);
+    Map<String, String> assets = const {},
+  }) : assets = Map<String, String>.of(assets);
 
-  TranslationAsset? operator [](String id) => assets[id];
-  void operator []=(String id, TranslationAsset asset) => assets[id] = asset;
+  String? operator [](String id) => assets[id];
+  void operator []=(String id, String asset) => assets[id] = asset;
+
+  String text(String id) => this[id] ?? '[MISSING TEXT]';
+  String actor(Actor actor) => this[actor.id] ?? '[MISSING ACTOR]';
+
+  String get title => this['title'] ?? '[MISSING TITLE]';
+  set title(String v) => this['title'] = v;
+
+  String get description => this['description'] ?? '[MISSING DESCRIPTION]';
+  set description(String v) => this['description'] = v;
+
+  String get tags => this['tags'] ?? '[MISSING TAGS]';
+  set tags(String v) => this['tags'] = v;
 
   factory Translation.fromJson(Map<String, dynamic> json) =>
       _$TranslationFromJson(json);
