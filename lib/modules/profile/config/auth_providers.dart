5044e123fd55fa39bd0fac9cf9af6bb5f333944e
Nursultan Askarbekuly <nursultan@outlook.com>
2022-04-03 22:57:43 +0300

    comment out the google sign-in

--- lib/modules/profile/config/auth_providers.dart
+++ lib/modules/profile/config/auth_providers.dart
@@ -3,5 +3,7 @@ import 'package:flutterfire_ui/auth.dart';
 
 final List<ProviderConfiguration> providerConfigs = [
   const EmailProviderConfiguration(),
+  // GoogleProviderConfiguration(
+      // clientId: DefaultFirebaseOptions.currentPlatform.appId),
   const AppleProviderConfiguration(),
 ];
