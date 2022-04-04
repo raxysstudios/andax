09f9259bbe858495e098b83a9aa359ebbbc54894
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2022-04-04 18:01:06 +0300

    Change client ID depending on platform

--- lib/config/auth_providers.dart
+++ lib/config/auth_providers.dart
@@ -1,10 +1,16 @@
+import 'dart:io' show Platform;
+
+import 'package:flutter/foundation.dart';
 import 'package:flutterfire_ui/auth.dart';
 
-const kGoogleClientId =
-    '803265650064-as3usri2iu8eba80tgdmrklvr2tvmtm6.apps.googleusercontent.com';
+// For some reason, the client ID for iOS is different than for Web and Android
+// The `kIsWeb` has to be the first check as `Platform` is not available in web
+final kGoogleClientId = kIsWeb || Platform.isAndroid
+    ? '803265650064-n8euv0sb8rcd1vrl8q49nj8p7e9qnr64.apps.googleusercontent.com'
+    : '803265650064-as3usri2iu8eba80tgdmrklvr2tvmtm6.apps.googleusercontent.com';
 
-const List<ProviderConfiguration> providerConfigs = [
-  EmailProviderConfiguration(),
+final List<ProviderConfiguration> providerConfigs = [
+  const EmailProviderConfiguration(),
   GoogleProviderConfiguration(clientId: kGoogleClientId),
-  AppleProviderConfiguration(),
+  const AppleProviderConfiguration(),
 ];
