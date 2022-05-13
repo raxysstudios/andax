0ea19f0213a7bf773e5d772027ffa72a281b2f02
alkaitagi <alkaitagi@outlook.com>
2022-05-13 18:51:32 +0300

    Removed elevation from like chip

--- lib/modules/home/widgets/like_chip.dart
+++ lib/modules/home/widgets/like_chip.dart
@@ -67,7 +67,6 @@ class _LikeChipState extends State<LikeChip> {
       avatar: Icon(
         liked ? Icons.favorite_rounded : Icons.favorite_border_rounded,
       ),
-      elevation: liked ? 3 : 0,
       label: Text(likes.toString()),
       onPressed: () => ensureSignIn(
         context,
